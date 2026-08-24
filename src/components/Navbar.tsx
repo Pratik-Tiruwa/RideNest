003fb9f5f6a698eee7a61a4351f1c6cf0c8757f0
Pratik Tiruwa <tiruwap408@gmail.com>
2026-08-24 22:27:44 +0530

    feat(navbar) : implemented animated navbar layout and add brand logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
+'use client'
 import React from 'react'
+import { motion } from 'motion/react' 
+import Image from 'next/image'
 
-const Navbar = () => {
-  return (
-    <div></div>
+
+function Navbar() { 
+  return ( 
+    <motion.div
+    initial = {{y:-60, opacity: 0}}
+    animate = {{y:0, opacity:1}}
+    className={`fixed top-3 left-1/2 -translate-x-1/2 
+      w-[94%] md:w-[85%] 
+      z-50 rounded-full bg-[#0B0B0B] text-white 
+      shadow-[0_15px_50px_rgba(0,0,0,0.7)] py-3`}
+    >
+      <div className='max-w-7xl mx-auto px-4 md:px-8 flex items-center justify-between'>
+         <Image src={"/logo.png"} alt='logo' width={44} height={44}/> 
+      </div>
+    </motion.div>
   )
 }
 
