ea6a8049d8ee6559064bb88f11c3f13055617957
Pratik Tiruwa <tiruwap408@gmail.com>
2026-09-07 10:53:23 +0530

    feat(home): mount Navbar, PublicHome, and Footer in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import PublicHome from "@/components/PublicHome";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="min-h-screen flex flex-col">
+      <Navbar />
+      <PublicHome />
+      <Footer />
+    </main>
   );
 }
+
